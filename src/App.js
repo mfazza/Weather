769640f15a898f264d4236378c69306f8505f42c
mattfazza <mathewsfazza@gmail.com>
2018-11-30 22:53:09 -0600

    Added class names and applied CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const API_KEY = "cf0fa652354f67ca276fa962a0572292";
 
@@ -29,7 +30,7 @@ class App extends React.Component {
     console.log(city + " " + country);
     
 
-    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + API_KEY + "&units=metric");
+    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + API_KEY + "&units=imperial");
     console.log(api_call);
 
     const data = await api_call.json();
@@ -40,13 +41,17 @@ class App extends React.Component {
       console.log("City and country exist");
       
       this.setState({
-        temperature: data.temp,
+        temperature: data.main.temp + "°F",
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
         description: data.weather[0].description,
         error: ""
     });
+
+    
+    
+
   } else {
       console.log("City and country do not exist");
       
@@ -69,22 +74,36 @@ class App extends React.Component {
 
 //About JSX: you can only return one parent element per render
 //getWeather is a prop.  When we add it to Form, we allow Form.js to use it.
-render(){
-  return(
+render() {
+  return (
     <div>
-        <Titles/>
-        <Form getWeather={this.getWeather}/>
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error = {this.state.error}      
-        />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Titles />
+              </div>
+              <div className="col-xs-6 form-container">
+                <Form getWeather={this.getWeather} />
+                <Weather 
+                  temperature={this.state.temperature} 
+                  humidity={this.state.humidity}
+                  city={this.state.city}
+                  country={this.state.country}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-}
+};
+
+
 
 export default App;
