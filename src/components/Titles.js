ae103b44900b6f27957a07c76838eb35e198c054
mattfazza <mathewsfazza@gmail.com>
2019-01-07 22:59:35 -0600

    Converted Titles.js into a stateless component

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-class Titles extends React.Component{
 
-    render() {
-        return(
-            <div>
-                <h1 className="title-container__title">Weather Finder</h1>
-                <p className="title-container__subtitle">Find out temperature, conditions, and more...</p>
-            </div>
-        );
-    }
+const Titles = () => {
+
+    return(
+        <div>
+            <h1 className="title-container__title">Weather Finder</h1>
+            <p className="title-container__subtitle">Find out temperature, conditions, and more...</p>
+        </div>
+    );
+
 }
 
 export default Titles;
