e05017c9c9693d895c883b9f3c8b5b686d9f287d
mattfazza <mathewsfazza@gmail.com>
2019-01-08 23:01:35 -0600

    Converted Forms into a stateless component, and fixed title

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,31 @@
 import React from "react";
 
-class Form extends React.Component{
+// class Form extends React.Component{
 
-    render(){
-        return(
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..."/>
-                <input type="text" name="country" placeholder="Country..."/>
-                <button>Get Weather</button>
-            </form>
-        );
-    }
+//     render(){
+//         return(
+//             <form onSubmit={this.props.getWeather}>
+//                 <input type="text" name="city" placeholder="City..."/>
+//                 <input type="text" name="country" placeholder="Country..."/>
+//                 <button>Get Weather</button>
+//             </form>
+//         );
+//     }
 
+// }
+
+
+//the conversion of this component into a stateless one is a bit different than Titles.js
+//the this keyword makes no sense without a class, and props need to be passed in to 
+//avoid being undefined
+const Form = (props) => {
+    return(
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="City..."/>
+            <input type="text" name="country" placeholder="Country..."/>
+            <button>Get Weather</button>
+        </form>
+    );
 }
 
 export default Form;
